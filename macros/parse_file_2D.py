b46997094b31cf9e033116585040d9f86ea97511
Manolis Kargiantoulakis <ekargian@fnal.gov>
2019-07-08 15:15:27 -0500

    last commit before changing to loaner machine

--- macros/parse_file_2D.py
+++ macros/parse_file_2D.py
@@ -18,13 +18,15 @@ z=data[:,2]
 V=data[:,3]
     
 # Open output file
-#f= open(filestem+'.dat',"w+")
-f= open('test.dat',"w+")
+f= open(filestem+'.dat',"w+")
+#f= open('test.dat',"w+")
 
 accepted_entries=0
 for i in range(len(r)):
-    if math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))<4.55 and abs(th[i]-6.45)<0.05 and abs(V[i])>0.01: 
-    #and math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))>2\ # perhaps avoid inner region, since the V formula is based on boundary conditions at edges
+    #if (abs(math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))-4.50)<0.2 or abs(math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))-3.50)<0.2 or abs(math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))-2.50)<0.2) and abs(th[i]-6.45)<0.05 and abs(V[i])>0.01: 
+    #if math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))<4.51 and math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))>4.49 and abs(V[i])>0.01: 
+    #if math.sqrt(math.pow(r[i]-711.2,2)+math.pow(z[i],2))<4.8:         
+    if i%20==0:
         for item in data[i]:
             f.write('%.11E  '%item, )
         f.write('\n')
